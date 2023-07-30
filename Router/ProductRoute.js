9245a075e359ff58d668c46bcb3063df3f5deae3
PraveenchandraR <118369839+PraveenchandraR@users.noreply.github.com>
2023-07-31 02:40:26 +0530

    Delete Router directory

--- Router/ProductRoute.js
+++
@@ -1,7 +0,0 @@
-const ProductRouter  = require("express").Router();
-const DetailController = require("../controller/DetailsController");
-
-
-ProductRouter.route("/:id").post(DetailController.findProduct);
-
-module.exports = ProductRouter;
--- Router/ProtectedRoute.js
+++
@@ -1,7 +0,0 @@
-const ProtectedRouter  = require("express").Router();
-const CartController = require("../controller/CartController");
-
-ProtectedRouter.route("/addItem").post(CartController.AddToCart);
-ProtectedRouter.route("/").post(CartController.showCart);
-
-module.exports = ProtectedRouter;
