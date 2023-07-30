a060c55338a0dbf4fc006f380500e37b464ff6bc
PraveenchandraR <118369839+PraveenchandraR@users.noreply.github.com>
2023-07-31 02:40:55 +0530

    Delete controller directory

--- controller/ElectronicController.js
+++
@@ -1,9 +0,0 @@
-const AllProduct = require("../Model/HomeModel");
-
-exports.Electronic= async(req,res)=>{
-    
-    const allData = await AllProduct.find({category:"electronics"});
-    res.status(200).json({
-        allData
-    });
-}
